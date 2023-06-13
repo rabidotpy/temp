169d656a8f82e336b8b3721d79d29482d25b5484
Rabi <46546705+rabidotpy@users.noreply.github.com>
2023-06-13 00:55:33 +0000

    update function.php

--- function.php
+++ function.php
@@ -190,6 +190,7 @@ function calculate_order_again_price($custom_meta){
     );
 
     $stickersMaterialPrices = array(
+        'Vinyl' => 0.108,
         'Vinyl Stickers' => 0.108,
         'Matte' => 0.108,
         'Holographic' => 0.139,
@@ -240,7 +241,7 @@ function calculate_order_again_price($custom_meta){
     
     
         $logger = wc_get_logger();
-        $context = array( 'source' => 'rabi_logs' );
+        $context = array( 'source' => 'rabi_logs_111' );
         $logger->error( "----------------------------------------" , $context );
         $logger->error( print_r($product_name, true) , $context );
         $logger->error( print_r($orderItemTotal, true) , $context );
@@ -265,19 +266,16 @@ function calculate_order_again_price($custom_meta){
 
 // Integrate your own hook
 add_action( 'woocommerce_cart_loaded_from_session', 'customized_wc_load_order_again_data' );
-
 function customized_wc_load_order_again_data() {
     global $woocommerce;
 
     if ( ! empty( $_GET['order_again'] ) && is_user_logged_in() && ( $order = wc_get_order( $_GET['order_again'] ) ) && $order->get_id() == $_GET['order_again'] ) {
-
         // Empty the current cart
         $woocommerce->cart->empty_cart();
 
         // Populate the cart with all products from the previous order
         foreach ( $order->get_items() as $item ) {
             if ( ( $product = $item->get_product() ) && $product->exists() ) {
-                
                 
                 $productName = $product->get_name(); // Here is where you get the product name.
                 $itemTotal = $item->get_total(); // get the item total
@@ -286,10 +284,15 @@ function customized_wc_load_order_again_data() {
                 // Load all product attributes
                 $variation = array();
                 $custom_meta = array();
+				$wapf_meta = array();
                 foreach ( $item->get_meta_data() as $meta ) {
                     $key = $meta->key;
                     if ( taxonomy_is_product_attribute( $key ) ) {
                         $variation[$key] = $item->get_meta( $key, true );
+                    }
+                    // Add a check for WAPF meta fields here
+                    if (strpos($key, 'wapf') !== false) { // Replace 'wapf' with the actual prefix or identifier used by WAPF
+                        $wapf_meta[$key] = $item->get_meta( $key, true );
                     }
                     $custom_meta[$key] = $item->get_meta( $key, true );
                 }
@@ -297,61 +300,49 @@ function customized_wc_load_order_again_data() {
                 $custom_meta["order_item_name_"] = $productName;
                 $custom_meta["Material"] = $item->get_meta('Material');
                 $custom_meta["Custom Price"] = $item->get_meta('Custom Price');
-                $calculatedPrice = calculate_order_again_price($custom_meta);
+                $calculatedPrice = calculate_order_again_price($custom_meta);     
                 $custom_meta["_order_again_price"] = $calculatedPrice;
-                // $logger = wc_get_logger();
-                // $context = array( 'source' => 'rabi_logs' );
-                // $logger->error( "----------------------------------------" , $context );
-                // $logger->error( print_r($custom_meta, true) , $context );
-                // $logger->error( "----------------------------------------" , $context );
-                // Add the product to the cart with the new price and custom meta data
-                $woocommerce->cart->add_to_cart( $product->get_id(), $quantity, $product instanceof WC_Product_Variation ? $product->get_parent_id() : 0, $variation, array('_order_again_price' => $calculatedPrice, 'custom_meta' => $custom_meta) );
+				
+				// Merge wapf_meta and custom_meta
+        		$meta_data = array_merge($wapf_meta, $custom_meta);
+                $logger = wc_get_logger();
+                $context = array( 'source' => 'rabi_new_1' );
+                $logger->error( "-----------------After-------------------" , $context );
+                $logger->error( print_r($item, true) , $context );
+                $logger->error( "------------------After End--------------------" , $context );
+				//Add the product to the cart with the new price and custom meta data
+                $woocommerce->cart->add_to_cart( 
+					$product->get_id(), 
+					$quantity, 
+					$product instanceof WC_Product_Variation ? $product->get_parent_id() : 0, 
+					$variation, 
+					array_merge(
+						array('_order_again_price' => $calculatedPrice, 'custom_meta' => $custom_meta), 
+						$wapf_meta
+					)
+				);
             }
         }
     }
 }
 
-add_action( 'woocommerce_after_cart_item_name', 'display_customized_item_data', 10, 2 );
-function display_customized_item_data( $cart_item, $cart_item_key ) {
-    $fields_to_display = array('cutline', 'width', 'height', 'material', 'quantity', 'custom price');
-    // Display only specified custom meta data
-    foreach ( $cart_item["custom_meta"] as $key => $value ) {
-        if ( in_array( strtolower($key), $fields_to_display ) && ! empty( $value ) ) {
-            if (strtolower($key) == 'quantity' && $value == $cart_item['quantity']) {
-                continue; // If quantity field matches the product quantity, skip it.
-            }
-        }
+add_action('woocommerce_admin_order_item_values', 'populate_wapf_fields', 1, 3);
+
+function populate_wapf_fields($product, $item, $item_id) {
+    // Retrieve the saved meta data
+    $custom_meta = wc_get_order_item_meta($item_id, 'custom_meta', true);
+    $logger = wc_get_logger();
+                $context = array( 'source' => 'edit_order' );
+                $logger->error(print_r($custom_meta, true) , $context );
+    // Check if the custom_meta was saved and if the WAPF fields should be populated
+    if ($custom_meta && isset($custom_meta['wapf_order_again']) && $custom_meta['wapf_order_again'] == 1) {
+        $logger = wc_get_logger();
+                $context = array( 'source' => 'edit_order' );
+                $logger->error( print_r($custom_meta['wapf_order_again']) , $context );
     }
 }
 
-add_filter( 'woocommerce_add_cart_item_data', 'add_custom_data_to_cart_item', 10, 2 );
-function add_custom_data_to_cart_item( $cart_item_data, $product_id ) {
-    $unique_cart_item_key = md5( microtime().rand() );
-    $cart_item_data['unique_key'] = $unique_cart_item_key;
-    return $cart_item_data;
-}
 
-
-
-
-
-// function set_custom_price_for_order_again_product( $cart ) {
-    
-//     if ( ! $cart instanceof WC_Cart ) {
-//         return;
-//     }
-
-//     // Iterate over each item in the cart
-//     foreach ( $cart->get_cart() as $cart_item ) {
-        
-//         // Check if the new_price is set
-//         if ( isset( $cart_item['new_price'] ) ) {
-            
-//             // Set the new price
-//             $cart_item['data']->set_price( (float) 103 );
-//         }
-//     }
-// }
 
 
 add_action( 'woocommerce_after_cart_item_name', 'display_custom_item_data', 10, 2 );
@@ -384,136 +375,46 @@ function display_custom_item_data( $cart_item, $cart_item_key ) {
 
 
 
-
-
-
-
-
-// Use the 'woocommerce_before_calculate_totals' hook to adjust the price
-add_action('woocommerce_before_calculate_totals', 'calculate_custom_price_for_the_order_again', 10);
-
-function calculate_custom_price_for_the_order_again($cart_object) {
-    
-    // echo "<pre>";
-    // print_r($cart_object);
-    // echo "</pre>";   
-    if ( ! $cart_object instanceof WC_Cart ) {
-        return;
-    }
-    //  echo "<pre>";
-    // print_r($cart_object);
-    // echo "</pre>";   
-  
-
-    foreach ($cart_object->get_cart() as $cart_item_key => $value) {
-        if ( isset( $value['custom_meta']['_order_again_price'] ) ) {
-            $value['data']->set_price( floatval( $value['custom_meta']['_order_again_price'] ) );
-        }
-    }
-}
-// function set_custom_price_for_order_again_product( $cart ) {
-//     if ( ! $cart instanceof WC_Cart ) {
-//         return;
-//     }
-    
-    
-
-//     // Ensure 'find_closest_lower_quantity' function exists
-//     if (!function_exists('find_closest_lower_quantity')) {
-//         echo "<!-- 'find_closest_lower_quantity' function doesn't exist -->";
-//         return;
-//     }
-
-//     // Prices and discounts data
-//     /* Note */
-//     //Change prices here or if you want to include another product just add an array like this here
-//     $labelsMaterialPrices = array(
-//         'Vinyl' => 0.084,
-//         'Matte' => 0.086,
-//         'Holographic' => 0.123,
-//         'Clear' => 0.086,
-//         'Heavy Duty' => 0.112,
-//         'Reflective' => 0.232,
-//         'Prismatic' => 0.158,
-//         'Glitter' => 0.158
-//     );
-
-//     $stickersMaterialPrices = array(
-//         'Vinyl' => 0.108,
-//         'Matte' => 0.108,
-//         'Holographic' => 0.139,
-//         'Clear' => 0.128,
-//         'Heavy Duty' => 0.175,
-//         'Reflective' => 0.175,
-//         'Prismatic' => 0.142,
-//         'Glitter' => 0.142
-//     );
-    
-//     /* Note */
-//     //If you have different discount for a different product then just copy this discount array, rename variable and use it in product's very own respective condition
-
-//     $discounts = array(
-//         1 => array('10' => 0, '50' => 0, '100' => 0, '200' => 0, '300' => 0, '500' => 0, '800' => 0, '1000' => 26, '1500' => 27, '2000' => 47, '5000' => 60),
-//         2 => array('10' => 0, '50' => 0, '100' => 0, '200' => 27, '300' => 27, '500' => 48, '800' => 55, '1000' => 55, '1500' => 62, '2000' => 69, '5000' => 75),
-//         3 => array('10' => 0, '50' => 0, '100' => 27, '200' => 48, '300' => 48, '500' => 55, '800' => 62, '1000' => 68, '1500' => 69, '2000' => 73, '5000' => 76),
-//         4 => array('10' => 0, '50' => 26, '100' => 53, '200' => 55, '300' => 62, '500' => 69, '800' => 69, '1000' => 74, '1500' => 74, '2000' => 77, '5000' => 81),
-//         5 => array('10' => 0, '50' => 25, '100' => 45, '200' => 59, '300' => 60, '500' => 67, '800' => 72, '1000' => 73, '1500' => 76, '2000' => 78, '5000' => 79),
-//         6 => array('10' => 0, '50' => 46, '100' => 53, '200' => 59, '300' => 67, '500' => 72, '800' => 72, '1000' => 76, '1500' => 78, '2000' => 78, '5000' => 81),
-//         7 => array('10' => 0, '50' => 44, '100' => 58, '200' => 67, '300' => 67, '500' => 72, '800' => 75, '1000' => 78, '1500' => 78, '2000' => 79, '5000' => 80),
-//         8 => array('10' => 0, '50' => 53, '100' => 59, '200' => 66, '300' => 72, '500' => 75, '800' => 78, '1000' => 78, '1500' => 79, '2000' => 79, '5000' => 80),
-//     );
-
-//     // Iterate over each item in the cart
-//     foreach ( $cart->get_cart() as $cart_item ) {
-//         $custom_meta = get_all_meta_values($cart_item['custom_meta']);
-//         $product_name = $cart_item['data']->get_name();
-//         $quantity = $custom_meta['Quantity'];
-//         $width = $custom_meta['Width'];
-//         $height = $custom_meta['Height'];
-//         $material = $cart_item['custom_meta']['Material'];
-//         $custom_price = $cart_item['custom_meta']['Custom Price'];
-//         // echo "custom_price ".$custom_price;
-//         $calculatedVals = 0;
-//         $widthClosestDiscount = intval(find_closest_lower_quantity(intval($quantity), $discounts[intval($width)]));
-//         $heightClosestDiscount = intval(find_closest_lower_quantity(intval($quantity), $discounts[intval($height)]));
-//         // echo "widthClosestDiscount ".$widthClosestDiscount;
-//         // echo "heightClosestDiscount ".$heightClosestDiscount;
-//         // echo "this is fuck ".intval($widthClosestDiscount > $heightClosestDiscount ? $widthClosestDiscount : $heightClosestDiscount );
-//         if ($product_name == "Print Custom Labels") {
-//             $calculatedVals = floatval($width) * floatval($height) * intval($quantity) * $labelsMaterialPrices[$material] * (((100 - intval($widthClosestDiscount > $heightClosestDiscount ? $widthClosestDiscount : $heightClosestDiscount )) / 100));
-//         } elseif ($product_name == "Print Custom Stickers") {
-//             $calculatedVals = floatval($width) * floatval($height) * intval($quantity) * $stickersMaterialPrices[$material] * (((100 - intval($widthClosestDiscount > $heightClosestDiscount ? $widthClosestDiscount : $heightClosestDiscount)) / 100));
-//         }
-
-//         $cart_item['data']->set_price((float)$calculatedVals);
-//     }
-// }
-
-// //Use custom Image on cart
-
+//Use custom Image on cart
 add_filter('woocommerce_cart_item_thumbnail', 'custom_cart_item_thumbnail', 10, 3);
-
 function custom_cart_item_thumbnail($thumbnail, $cart_item, $cart_item_key) {
     if (isset($cart_item['custom_meta']['Image'])) {
         $image_url = $cart_item['custom_meta']['Image'];
-        return '<img src="' . esc_url($image_url) . '" alt="">';
+        $file_type = wp_check_filetype( $image_url );
+
+        if (in_array( $file_type['ext'], array('jpg', 'jpeg', 'png') )) {
+            return '<img src="' . esc_url($image_url) . '" alt="">';
+        } else {
+            return $thumbnail;
+        }
     } else {
         return $thumbnail;
     }
 }
 
 
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// //Disable order again button
+// remove_action( 'woocommerce_order_details_after_order_table', 'woocommerce_order_again_button' );
+
+
+
+
+
+
+
+
+
 //Use custom image on checkout page as well
 
-add_filter('woocommerce_checkout_cart_item_quantity', 'custom_checkout_cart_item_thumbnail', 10, 3);
+// add_filter('woocommerce_cart_item_name', 'custom_checkout_cart_item_thumbnail', 10, 3);
 
-function custom_checkout_cart_item_thumbnail($quantity_html, $cart_item, $cart_item_key) {
-    if (isset($cart_item['custom_meta']['Image'])) {
-        $image_url = $cart_item['custom_meta']['Image'];
-        $image_html = '<img src="' . esc_url($image_url) . '" alt="" class="checkout-product-thumbnail">';
-        return $image_html . $quantity_html;
-    } else {
-        return $quantity_html;
-    }
-}
-
+// function custom_checkout_cart_item_thumbnail($quantity_html, $cart_item, $cart_item_key) {
+//     if (isset($cart_item['custom_meta']['Image'])) {
+//         $image_url = $cart_item['custom_meta']['Image'];
+//         $image_html = '<img src="' . esc_url($image_url) . '" alt="" class="checkout-product-thumbnail">';
+//         return $image_html . $quantity_html;
+//     } else {
+//         return $quantity_html;
+//     }
+// }
